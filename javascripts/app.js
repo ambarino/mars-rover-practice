1537cc48c54c01e721515f0222b62b58b086108a
Teo <teodorolg130@gmail.com>
2019-04-13 14:03:21 +0200

    Incluido sistema de turnos

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,197 +1,210 @@
-// Rover Object Goes Here
-// ======================
 var grid = [
-                      //NORTH
-           [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-           [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-   /*WEST*/[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //EAST
-           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];                    //SOUTH
+                   //NORTH
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+/*WEST*/[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //EAST
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];                 //SOUTH
 
-var _X = 0;
-var _Y = 0;
-var rover = {
+var turn = 1;
+
+var rover1 = {
+  name: "Rover1",
+  _X: 0,
+  _Y: 0,
   direction: "N",
-  position: [[_X], [_Y]],
   travelLog: [[0, 0]]
 }
 
-// ======================
-
-
-function turnLeft(rover){
-  switch(rover.direction){
-    case "N":
-      rover.direction = "W";
-      break;
-    case "W":
-      rover.direction = "S";
-      break;
-    case "S":
-      rover.direction = "E";
-      break;
-    case "E":
-      rover.direction = "N";
-      break;
-  }
-  console.log("The Rover is now heading " + rover.direction);
+var rover2 = {
+  name: "Rover2",
+  _X: 10,
+  _Y: 10,
+  direction: "N",
+  travelLog: [[10, 10]]
 }
 
-function turnRight(rover){
-  switch(rover.direction){
-    case "N":
-      rover.direction = "E";
-      break;
-    case "W":
-      rover.direction = "N";
-      break;
-    case "S":
-      rover.direction = "W";
-      break;
-    case "E":
-      rover.direction = "S";
-      break;
+function order(orden){
+  if (turn===1){
+    var queRover = rover1;
+    moveRover(orden, queRover);
+    turn++;
+  } else if (turn===2){
+    var queRover = rover2;
+    moveRover(orden, queRover);
+    turn--;
   }
-  console.log("The Rover is now heading " + rover.direction);
 }
 
-function moveForward(rover){
-  switch(rover.direction){
-    case "N":
-     if(_X>0){  
-        _X--;
-        updatePosition(rover);
-        checkObstacles(rover);
-        break;
-      } else {
-        console.log("El Rover no puede moverse fuera de la rejilla")
-        break;
-      }
-    case "W":
-     if(_Y>0){
-        _Y--;
-        updatePosition(rover);
-        checkObstacles(rover);
-        break;
-      } else {
-        console.log("El Rover no puede moverse fuera de la rejilla")
-        break;
-      }
-    case "S":
-      if(_X<10){
-        _X++;
-        updatePosition(rover);
-        checkObstacles(rover);
-        break;
-      } else {
-        console.log("El Rover no puede moverse fuera de la rejilla")
-        break;
-      }
-    case "E":
-     if(_Y<10){
-        _Y++;
-        updatePosition(rover);
-        checkObstacles(rover);
-        break;
-      } else {
-        console.log("El Rover no puede moverse fuera de la rejilla")
-        break;
-      }
-  }
-  console.log("The Rover is now at " + "["+_X+", "+_Y+"]");
-  recordLog(rover);
+function moveRover(orden, queRover){
+  grid[queRover._X][queRover._Y] = 0;
+  movement(orden, queRover);
+  grid[queRover._X][queRover._Y] = 2;
+  console.log("The " + queRover.name + " has travelled through: ");
+  console.log(queRover.travelLog);
 }
 
-function moveBackward(rover){
-  switch(rover.direction){
-    case "N":
-      if(_Y<10){
-        _Y++;
-        updatePosition(rover);
-        checkObstacles(rover);
-        break;
-      } else {
-        console.log("El Rover no puede moverse fuera de la rejilla")
-        break;
-      }
-    case "W":
-      if(_X<10){
-        _X++;
-        updatePosition(rover);
-        checkObstacles(rover);
-        break;
-      } else {
-        console.log("El Rover no puede moverse fuera de la rejilla")
-        break;
-      }
-    case "S":
-      if(_Y>0){  
-        _Y--;
-        updatePosition(rover);
-        checkObstacles(rover);
-        break;
-      } else {
-        console.log("El Rover no puede moverse fuera de la rejilla")
-        break;
-      }
-    case "E":
-      if(_X>0){
-        _X--;
-        updatePosition(rover);
-        checkObstacles(rover);
-        break;
-      } else {
-        console.log("El Rover no puede moverse fuera de la rejilla")
-        break;
-      }
-  }
-  console.log("The Rover is now at " + "["+_X+", "+_Y+"]");
-  recordLog(rover);
+function updateTravelLog(queRover){
+  queRover.travelLog.push([queRover._X, queRover._Y]);
 }
 
-function movement(order){
- for (var i = 0; i<order.length; i++){ 
-  switch(order[i]){
-    case "f":
-      moveForward(rover);
-      break;
-    case "b":
-      moveBackward(rover);
-      break;
-    case "r":
-      turnRight(rover);
-      break;
-    case "l":
-      turnLeft(rover);
-      break;
-    default:
-      console.log("Debe introducir un valor valido para el Rover. \n f: Mueve hacia delante \n b: Mueve hacia atras \n r: Gira a la derecha \n l: Mueve a la izquierda.")
-      i=order.length;
-      break;
+function checkObstacles(queRover){
+  if (grid[queRover._X][queRover._Y] === 1){
+    queRover._X = queRover.travelLog[queRover.travelLog.length-1][0];
+    queRover._Y = queRover.travelLog[queRover.travelLog.length-1][1];
+    console.log("There's an obstacle in the way, the Rover can't move through here.");
+  } else if(grid[queRover._X][queRover._Y] === 2){
+    queRover._X = queRover.travelLog[queRover.travelLog.length-1][0];
+    queRover._Y = queRover.travelLog[queRover.travelLog.length-1][1];
+    console.log("There's another Rover in the way, the Rover can't move through here.");    
+  }
+}
+
+function movement(orden, queRover){
+  for (var i = 0; i<orden.length; i++){ 
+  updateTravelLog(queRover);
+     switch(orden[i]){
+     case "f":
+       moveForward(queRover);
+       break;
+     case "b":
+       moveBackward(queRover);
+       break;
+     case "r":
+       turnRight(queRover);
+       break;
+     case "l":
+       turnLeft(queRover);
+       break;
+     default:
+       console.log("Debe introducir un valor valido para el Rover. \n f: Mueve hacia delante \n b: Mueve hacia atras \n r: Gira a la derecha \n l: Mueve a la izquierda.")
+       i=orden.length;
+       break;
+   }
+   console.log("The " + queRover.name + " is at " + "[" +queRover._X +", " + queRover._Y +"]");
   }
  }
- console.log("El Rover ha viajado a través de las siguientes coordenadas");
- console.log(rover.travelLog);
-}  
 
-function recordLog(rover){
-  rover.travelLog.push([_X, _Y])
+function turnLeft(queRover){
+  switch(queRover.direction){
+    case "N":
+      queRover.direction = "W";
+      break;
+    case "W":
+      queRover.direction = "S";
+      break;
+    case "S":
+      queRover.direction = "E";
+      break;
+    case "E":
+      queRover.direction = "N";
+      break;
+  }
+  console.log("The " + queRover.name + " is now heading " + queRover.direction);
 }
 
-function checkObstacles(rover){
-  if(grid[_X][_Y]===1){
-    console.log("Hay un obstaculo en el camino, el Rover no puede desplazarse en esa dirección.");
-    _X=rover.travelLog[rover.travelLog.length-1][0];
-    _Y=rover.travelLog[rover.travelLog.length-1][1];    
+function turnRight(queRover){
+  switch(queRover.direction){
+    case "N":
+      queRover.direction = "E";
+      break;
+    case "W":
+      queRover.direction = "N";
+      break;
+    case "S":
+      queRover.direction = "W";
+      break;
+    case "E":
+      queRover.direction = "S";
+      break;
+  }
+  console.log("The " + queRover.name +  " is now heading " + queRover.direction);
+}
+
+function moveForward(queRover){
+  switch(queRover.direction){
+    case "N":
+     if(queRover._X>0){  
+        queRover._X--;
+        checkObstacles(queRover);
+        break;
+      } else {
+        console.log("El Rover no puede moverse fuera de la rejilla")
+        break;
+      }
+    case "W":
+     if(queRover._Y>0){
+        queRover._Y--;
+        checkObstacles(queRover);
+        break;
+      } else {
+        console.log("El Rover no puede moverse fuera de la rejilla")
+        break;
+      }
+    case "S":
+      if(queRover._X<10){
+        queRover._X++;
+        checkObstacles(queRover);
+        break;
+      } else {
+        console.log("El Rover no puede moverse fuera de la rejilla")
+        break;
+      }
+    case "E":
+     if(queRover._Y<10){
+        queRover._Y++;
+        checkObstacles(queRover);
+        break;
+      } else {
+        console.log("El Rover no puede moverse fuera de la rejilla")
+        break;
+      }
   }
 }
 
-function updatePosition(rover){
-  rover.position= [[_X],[_Y]];
-}
+function moveBackward(queRover){
+  switch(queRover.direction){
+    case "N":
+      if(queRover._Y<10){
+        queRover._Y++;
+        checkObstacles(queRover);
+        break;
+      } else {
+        console.log("El Rover no puede moverse fuera de la rejilla")
+        break;
+      }
+    case "W":
+      if(queRover._X<10){
+        queRover._X++;
+        checkObstacles(queRover);
+        break;
+      } else {
+        console.log("El Rover no puede moverse fuera de la rejilla")
+        break;
+      }
+    case "S":
+      if(queRover._Y>0){  
+        queRover._Y--;
+        checkObstacles(queRover);
+        break;
+      } else {
+        console.log("El Rover no puede moverse fuera de la rejilla")
+        break;
+      }
+    case "E":
+      if(queRover._X>0){
+        queRover._X--;
+        checkObstacles(queRover);
+        break;
+      } else {
+        console.log("El Rover no puede moverse fuera de la rejilla")
+        break;
+      }
+  }
+ }
